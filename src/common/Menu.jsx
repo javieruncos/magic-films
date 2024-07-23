5cd0c54bca35028cf44c08aa4a49bab208b93e7e
Javier Uncos <javiersrfc30@gmail.com>
2024-07-23 17:40:22 -0300

    agregamos estilos al menu y correcioes de estilos

--- src/common/Menu.jsx
+++ src/common/Menu.jsx
@@ -10,8 +10,10 @@ const Menu = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="#home" className="link-menu">Home</Nav.Link>
+                            <Nav.Link href="#link" className="link-menu">Sobre Nosotros</Nav.Link>
+                            <Nav.Link href="#home" className="link-menu">Servicios</Nav.Link>
+                            <Nav.Link href="#link" className="link-menu">Contacto</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
