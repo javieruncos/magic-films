bfb924cc382850ae14300f0b0aa48fda90176ed9
Javier Uncos <javiersrfc30@gmail.com>
2024-07-29 19:37:28 -0300

    agregamos estilos responsive al slider de cometarios

--- src/components/Ui/SliderComents.jsx
+++ src/components/Ui/SliderComents.jsx
@@ -21,6 +21,23 @@ const SliderComents = () => {
                     pagination={{
                         clickable: true,
                     }}
+                    breakpoints={{
+                        
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                        },
+                       
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                      
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                        },
+                    }}
                     modules={[FreeMode, Pagination]}
                     className="mySwiper"
                 >
